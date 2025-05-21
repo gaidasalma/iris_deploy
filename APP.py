b886cd74b1272d51c955845c3bb8bfb43e41e703
gaidasalma <gaidaaqilla@students.unnes.ac.id>
2025-05-21 14:58:09 +0700

    Update APP.py

--- APP.py
+++ APP.py
@@ -54,9 +54,11 @@ elif page == "Prediksi":
 
     input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
 
-    if st.button("Prediksi"):
-        pred = model.predict(input_data)[0]
-        st.success(f"🌸 Prediksi: **{target_names[pred]}**")
+   if st.button("Prediksi"):
+    pred = model.predict(input_data)
+    pred_int = int(pred[0])  # pastikan ini integer
+    pred_label = target_names[pred_int]
+    st.success(f"🌸 Prediksi: **{pred_label}**")
 
 # Halaman 3: Visualisasi
 elif page == "Visualisasi":
